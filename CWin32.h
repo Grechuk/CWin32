d4644d4652196fa30c4ce9a264723e38415b8ed7
Grechuk <30843774+Grechuk@users.noreply.github.com>
2017-08-08 19:57:11 -0300

    Add files via upload

---
+++ CWin32.h
@@ -0,0 +1,18 @@
+//-------------------------------------------------------------------------------
+//  Arquivo no qual todas as classes da CWin32 convergem e são incluídas.
+//
+//  Criado por Gabriel Grechuk da Silva Oliveira.
+//    <gabrielgrechuk@outlook.com>
+//
+//  Data: 17:40  06/08/2017.
+//
+
+#ifndef _CWIN32_H_
+#define _CWIN32_H_
+
+//-------------------------------------------------------------------------------
+// Arquivos pra inclusão.
+#include <windows.h>
+#include <tchar.h>
+
+#endif
